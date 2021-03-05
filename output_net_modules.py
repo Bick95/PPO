61bdfa1717aa64148161f6e54a35be3c520a399e
Bick95 <daniel.bick@live.de>
2021-03-05 22:33:36 +0100

    Continued proof-reading; added some prints

--- output_net_modules.py
+++ output_net_modules.py
@@ -8,17 +8,18 @@ class OutMLP(nn.Module):
 
     def __init__(self,
                  output_features: int,
-                 hidden_features: int = 50,
+                 input_features: int = 50,
                  output_type: int = DISCRETE
                  ):
         super(OutMLP, self).__init__()
 
         # Construct NN-processing pipeline consisting of concatenation of layers to be applied to any input
+        # (Avoids if-statements for whether to apply the Softmax in forward pass)
         self.pipeline = [
 
             # Add output layer
             nn.Linear(
-                in_features=hidden_features,
+                in_features=input_features,
                 out_features=output_features
             )
 
@@ -28,7 +29,7 @@ class OutMLP(nn.Module):
         for i, layer in enumerate(self.pipeline):
             self.add_module("layer_mlp_out_" + str(i), layer)
 
-        # Add optional normalization of outputs
+        # Add optional normalization of outputs in case of Discrete distribution over action space
         if output_type is DISCRETE:
             self.pipeline.append(nn.Softmax(dim=1))
 
