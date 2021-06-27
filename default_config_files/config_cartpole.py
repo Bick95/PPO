d24f448e8c6bdf199fd789d39bf1906e1c6433a8
Bick95 <daniel.bick@live.de>
2021-06-27 22:57:51 +0200

    Found super stable default config for cartpole. Performance seemed degraded previously due to dimensionality error of log-probs in the code.

--- default_config_files/config_cartpole.py
+++ default_config_files/config_cartpole.py
@@ -4,37 +4,24 @@
     'grayscale_transform': False,
     'markov_length': 4,
     'dilation': 1,
-    "total_num_state_transitions": 1000000,
+    "total_num_state_transitions": 200000,
     "param_sharing": True,
 
     "epochs": 5,
     "parallel_agents": 5,
-    "learning_rate_pol": {
-        'decay_type': 'exponential',
-        'decay_rate': 0.99,
-        'initial_value': 0.00002,
-        'min_value': 0.000008,
-        'verbose': True,
-    },
-    "learning_rate_val": {
-        'decay_type': 'exponential',
-        'decay_rate': 0.98,
-        'initial_value': 0.00001,
-        'min_value': 0.000008,
-        'verbose': True,
-    },
-
-    "clipping_parameter": {
-        'decay_type': 'linear',
-        #'decay_rate': 0.9,
-        'initial_value': 0.15,
-        'min_value': 0.0005,
-        'verbose': True,
-    },
-
     "trajectory_length": 600,
     "discount_factor": 0.99,
     "batch_size": 32,
+
+    "learning_rate_pol": 0.0002,
+    "learning_rate_val": 0.0002,
+
+    "clipping_parameter": {
+        'decay_type': 'linear',
+        'initial_value': 0.15,
+        'min_value': 0.005,
+        'verbose': True,
+    },
 
     "entropy_contrib_factor": 0.01,
     "vf_contrib_factor": 1.,
@@ -42,13 +29,12 @@
     'show_final_demo': True,
     'frame_duration': 0.08,  # Indicates how long rendered frames are shown to yield a nice and smooth visualization
     'max_render_time_steps': 300,  # For how many time steps to render the environment during finl demo
+
     'deterministic_eval': True,
     'stochastic_eval': True,
-    
     'intermediate_eval_steps': 100,
     'time_steps_extensive_eval': 10000,
-    
-    #'standard_dev': torch.ones,  # only used for continuous action spaces
+
     'network_structure': [
 		# Nr. of nodes for fully connected layers
 		32,
